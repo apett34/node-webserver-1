4e8b76e1ffaa4e9b9e5c6624e71bdfe740ecbd37
Arild Pettersen <apett34@gmail.com>
2018-03-05 18:08:03 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const hbs = require('hbs'); // Handlebars
+const fs = require('fs'); 	// File System
+
+var	app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+// Create some middelware
+app.use((req, res, next) => {
+	var log = '';
+	var now = new Date().toString();
+	log = now;
+	log += ': '
+	log += req.method;
+	log += ' ';
+	log += req.url;
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if (err) {
+			console.log('Unable to append to server.log');
+		}
+	});
+	next();
+});
+
+
+// Create some middelware
+/*
+app.use((req, res, next) => {
+	res.render('maintenance.hbs');
+});
+*/
+
+app.use(express.static(__dirname + '/public')); // app.use registrerer middleware
+
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
+app.get('/', function(req, res){		// registrer en handler
+	console.log('Root page visited.');
+	res.render('home.hbs', {
+		welcomeMessage: 'Hello World!',
+		pageTitle: 'Home Page'
+	});
+
+});
+
+
+app.get('/about', function(req, res){
+	console.log('About page visited.');
+	res.render('about.hbs', {
+		pageTitle: 'About Page'
+	});
+});
+
+
+var	server = app.listen(8081, function(){
+	var	host = server.address().address;
+	var port = server.address().port;
+	console.log("Server listening at http://%s:%s", host, port);
+});
